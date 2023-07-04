b262f586cbdaeac9a568b4384f2c398ed1c0f32c
Amanda <amandarankin@macbook-pro.myfiosgateway.com>
2023-07-04 17:07:58 -0400

    mvp

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@ import styles from "./page.module.css";
 import React, { useCallback, useEffect, useState, useMemo } from "react";
 import moment from "moment";
 import { Members, members } from "@/members";
+import useSound from "use-sound";
 import _ from "lodash";
 
 type Groups = {
@@ -10,28 +11,26 @@ type Groups = {
   group2: string[];
 };
 
-
-
 const Home: React.FC = () => {
   const [language, setLanguage] = useState<"en" | "es" | null>(null);
   const [timeDate, setTimeDate] = useState<Date>(new Date());
   const [todaysMembers, setTodaysMembers] = useState<Members>(members);
   const [breakoutGroups, setBreakoutGroups] = useState<Groups | null>(null);
   const [timeRemaining, setTimeRemaining] = useState<number | null>(null);
-  const [currentSegment, setCurrentSegment] = useState<'greetings' | 'breakout' | 'farewells'>('greetings')
-  const [currentSegmentText, setCurrentSegmentText] = useState<string>('')
-  const [breakoutSession, setBreakoutSession] = useState<'breakout1' | 'breakout2' | null>(null)
+  const [play, { stop }] = useSound("bell.wav");
 
-  const text: (english: string, spanish:string) => string = useCallback((english: string , spanish: string) => {
-    if(!language) {
-      return ''
-    }
-    if(language === 'en') {
-      return english
-    }
-    return spanish
-
-  }, [language])
+  const text: (english: string, spanish: string) => string = useCallback(
+    (english: string, spanish: string) => {
+      if (!language) {
+        return "";
+      }
+      if (language === "en") {
+        return english;
+      }
+      return spanish;
+    },
+    [language]
+  );
 
   const handleCheckboxChange = (memberName: string, language: string) => {
     setTodaysMembers((prevMembers: Members) => ({
@@ -57,7 +56,7 @@ const Home: React.FC = () => {
   useEffect(() => {
     const interval = setInterval(() => {
       setTimeDate(new Date());
-    }, 60000); 
+    }, 1000 * 30);
 
     return () => {
       clearInterval(interval);
@@ -65,28 +64,10 @@ const Home: React.FC = () => {
   }, []);
 
   useEffect(() => {
-    if(timeDate.getMinutes() >= 55) {
-      setCurrentSegment('farewells')
+    if (timeDate.getMinutes() === 30 || timeDate.getMinutes() === 55) {
+      play();
     }
-  }, [currentSegment, timeDate])
-
-
-  useEffect(() => {
-    switch (currentSegment) {
-      case 'greetings':
-        setCurrentSegmentText(text('greetings', 'saludos'))
-        break;
-      case 'breakout':
-        setCurrentSegmentText(text('breakout groups', 'grupos de discusión'))
-        break;
-      case 'farewells':
-        setCurrentSegmentText(text('farewells', 'despedidas'))
-        break;
-      default:
-        setCurrentSegmentText('')
-    }
-    
-  }, [currentSegment, text, setCurrentSegment])
+  }, [timeDate, play]);
 
   const createBreakoutGroups = useCallback(() => {
     const { english, spanish } = todaysMembers;
@@ -116,123 +97,129 @@ const Home: React.FC = () => {
     };
 
     setBreakoutGroups(groups);
-    setCurrentSegment('breakout');
-
-    switch(breakoutSession) {
-      case 'breakout1':
-        setBreakoutSession('breakout2')
-      default:
-        setBreakoutSession('breakout1')
-    }
-  
-  }, [todaysMembers, breakoutSession]);
-
- 
+  }, [todaysMembers]);
 
   const reset = useCallback(() => {
-    setBreakoutGroups(null)
-    setCurrentSegment('greetings')
-  }, [setBreakoutGroups])
+    setBreakoutGroups(null);
+    setTodaysMembers(members);
+  }, [setBreakoutGroups]);
 
   useEffect(() => {
     const calculateTimeRemaining = () => {
       const currentTime = new Date();
       const minutes = currentTime.getMinutes();
-
-      if (currentSegment === 'farewells') {
-        const remainingMinutes = 60 - minutes - 1; // Subtract 1 for the current minute
-        setTimeRemaining(remainingMinutes);
-      } else if (currentSegment === 'breakout') {
-        if (breakoutSession === 'breakout1' || breakoutSession === 'breakout2') {
-          const remainingMinutes = Math.floor((53 - minutes - 1) / 2); // Subtract 1 for the current minute
-          setTimeRemaining(remainingMinutes);
-        } else {
-          setTimeRemaining(0);
-        }
+      const remaining = 55 - minutes;
+      if (remaining > 0) {
+        setTimeRemaining(remaining);
       } else {
-        setTimeRemaining(0);
+        setTimeRemaining(null);
+        setBreakoutGroups(null);
       }
     };
 
     calculateTimeRemaining();
-
     const timerInterval = setInterval(() => {
       calculateTimeRemaining();
-    }, 1000); // Update every second
+    }, 1000 * 60); // Update every minute
 
     return () => {
       clearInterval(timerInterval);
     };
-  }, [currentSegment, breakoutSession]);
+  }, []);
 
   return (
     <div className={styles.content}>
       <main className={styles.main}>
-        <button onClick={reset}>{text('New session', 'Nueva sesión')}</button>
         <h1>Las Violetas</h1>
-        {language && (
-          <>
-            <h2>{text('Book club', 'Club de lectura')}</h2>
-            <div>
-              <strong>{text('Currently reading', 'En la lectura actua')}:</strong>{' '}{text('The Paris Apartment', 'Un apartamento en Paris')}
-            </div>
 
-            <div className={styles.sessionInfo}>
-              <input
-                type="datetime-local"
-                value={
-                  timeDate ? moment(timeDate).format("YYYY-MM-DDTHH:mm") : ""
-                }
-                onChange={(e) => setTimeDate(moment(e.target.value).toDate())}
-              />
-              <ul>
-              <li>{text('Current language', 'Idioma actual')}{': '}{text('English', 'español')}</li>
-              <li>{text('Current segment', 'Segmento en curso')}{': '}{currentSegmentText}</li>
-              <li>{text('Time remaining in segment', 'Tiempo restante en el segmento')}{': '}{timeRemaining}</li>
-              </ul>
+        <h2>{text("Book club", "Club de lectura")}</h2>
+        <div className={styles.currentReading}>
+          <strong>{text("Currently reading", "En la lectura actual")}:</strong>{" "}
+          {text("The Paris Apartment", "Un apartamento en Paris")}
+        </div>
+
+        <div className={styles.dateSetter}>
+          <input
+            type="datetime-local"
+            value={timeDate ? moment(timeDate).format("YYYY-MM-DDTHH:mm") : ""}
+            onChange={(e) => setTimeDate(moment(e.target.value).toDate())}
+          />
+        </div>
+        {!breakoutGroups && (
+          <div className={styles.session}>
+            <div className={styles.members}>
+              <div className={styles.memberGroup}>
+                <h3>{text("Native English speakers", "Anglohablantes")}</h3>
+                <ul>
+                  {members.english.map((name) => (
+                    <li key={name}>
+                      <input
+                        type="checkbox"
+                        checked={todaysMembers.english.includes(name)}
+                        onChange={() => handleCheckboxChange(name, "english")}
+                      />
+                      <label>{name}</label>
+                    </li>
+                  ))}
+                </ul>
+              </div>
+              <div className={styles.memberGroup}>
+                <h3>{text("Native Spanish speakers", "Hispanohablantes")}</h3>
+                <ul>
+                  {members.spanish.map((name) => (
+                    <li key={name}>
+                      <input
+                        type="checkbox"
+                        checked={todaysMembers.spanish.includes(name)}
+                        onChange={() => handleCheckboxChange(name, "spanish")}
+                      />
+                      <label>{name}</label>
+                    </li>
+                  ))}
+                </ul>
+              </div>
             </div>
-            <h3>
-              {text('Native English speakers', 'Anglohablantes')}
-            </h3>
-            {members.english.map((name) => (
-              <div key={name}>
-                <input
-                  type="checkbox"
-                  checked={todaysMembers.english.includes(name)}
-                  onChange={() => handleCheckboxChange(name, "english")}
-                />
-                <label>{name}</label>
-              </div>
-            ))}
-            <h3>
-                 {text('Native Spanish speakers', 'Hispanohablantes')}
-            </h3>
-            {members.spanish.map((name) => (
-              <div key={name}>
-                <input
-                  type="checkbox"
-                  checked={todaysMembers.spanish.includes(name)}
-                  onChange={() => handleCheckboxChange(name, "spanish")}
-                />
-                <label>{name}</label>
-              </div>
-            ))}
             <button onClick={createBreakoutGroups}>
-            {text('Create breakout groups', 'Crear grupos de discusión')}
+              {text("Create groups", "Crear grupos")}
             </button>
-            {breakoutGroups && (
-              <div>
-                <h3>{text('Breakout groups', 'Grupos de discusión')}</h3>
-                <strong>Group 1:</strong>
-                {breakoutGroups.group1.map((member) => (
-                  <li key={member}>{member}</li>
-                ))}
-                <strong>Group 2:</strong>
-                {breakoutGroups.group2.map((member) => (
-                  <li key={member}>{member}</li>
-                ))}
+          </div>
+        )}
+
+        {breakoutGroups && (
+          <>
+        <h3>{text("Breakout groups", "Grupos de discusión")}</h3>
+          <div className={styles.breakouts}>
+            <div className={styles.members}>
+              <div className={styles.memberGroup}>
+                <h4>Group 1:</h4>
+                <ul>
+                  {breakoutGroups.group1.map((member) => (
+                    <li key={member}>{member}</li>
+                  ))}
+                </ul>
               </div>
-            )}
+              <div className={styles.memberGroup}>
+                <h4>Group 2:</h4>
+                <ul>
+                  {breakoutGroups.group2.map((member) => (
+                    <li key={member}>{member}</li>
+                  ))}
+                </ul>
+              </div>
+            </div>
+            <div className={styles.timer}>
+              <label>
+              {text(
+                "Time remaining in groups",
+                "Tiempo restante en grupos"
+              )}{": "}</label>
+              <div className={styles.counter}>{timeRemaining}</div>
+            </div>
+            </div>
+            
+            <button onClick={reset}>
+            {text("New session", "Nueva sesión")}
+          </button>
           </>
         )}
       </main>
